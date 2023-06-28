75dd28fe525cd5f0fca92f936aae6c84588496e3
AlexanderB1994 <bulgaru.alexander1994@gmail.com>
2023-06-28 22:58:51 +0300

    v5 update

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from cars.models import BodyType, Brand, CarModel, Car
 
-# Register your models here.
+
+@admin.register(BodyType)
+class BodyTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    @admin.display(description="body type")
+    def get_body_type(self, obj):
+        return ", ".join([str(body_type) for body_type in obj.body_type.all()])
+
+    list_display = ("name", "get_body_type")
+
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "brand", "body_type")
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    @admin.display(description="body type")
+    def get_body_type(self, obj):
+        return str(obj.model.body_type)
+
+    list_display = (
+        "model", "get_body_type", "year", "engine", "fuel_type", "cost_annual", "major_repair", "fuel_consumption")
--- cars/models.py
+++ cars/models.py
@@ -4,16 +4,26 @@ from django.db import models
 class BodyType(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
     body_type = models.ManyToManyField(BodyType)
+
+    def __str__(self):
+        return self.name
 
 
 class CarModel(models.Model):
     name = models.CharField(max_length=100)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     body_type = models.ForeignKey(BodyType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
@@ -23,3 +33,6 @@ class Car(models.Model):
     fuel_consumption = models.FloatField()
     cost_annual = models.FloatField()
     major_repair = models.FloatField()
+
+    def __str__(self):
+        return self.model.name
